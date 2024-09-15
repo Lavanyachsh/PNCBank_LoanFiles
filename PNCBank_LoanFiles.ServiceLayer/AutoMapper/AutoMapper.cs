d74d2606321a78909401d7fc1d56cea392293c94
lavanyachsh <lavanyachsh@gmail.com>
2024-09-15 11:37:23 +0530

    Add project files.

---
+++ PNCBank_LoanFiles.ServiceLayer/AutoMapper/AutoMapper.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PNCBank_LoanFiles.BusinessEntities;
+using PNCBank_LoanFiles.RepositoryLayer;
+
+namespace PNCBank_LoanFiles.ServiceLayer
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile() 
+        {
+            CreateMap<FileUploadDTO, FileUpload>();
+            CreateMap<FileUpload, FileUploadDTO>();
+
+            //CreateMap<PgAccountDTO, PgAccount>();
+            //CreateMap<PgAccount, PgAccountDTO>();
+        }
+    }
+}
---
+++ PNCBank_LoanFiles.ServiceLayer/Services/FilesUploadService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PNCBank_LoanFiles.RepositoryLayer;
+using PNCBank_LoanFiles.BusinessEntities;
+using PNCBank_LoanFiles.BusinessEntities.Interfaces;
+using AutoMapper;
+using System.Security.Principal;
+namespace PNCBank_LoanFiles.ServiceLayer.Services
+{
+    public class FilesUploadService : IFilesUploadService
+    {
+        public readonly IFilesUploadRepository _filesUploadRepository;
+        private readonly IMapper _mapper;
+        public FilesUploadService(IFilesUploadRepository filesUploadRepository, IMapper mapper)
+        {
+            this._filesUploadRepository = filesUploadRepository;
+            this._mapper = mapper;
+        }
+        public  async Task<List<FileUploadDTO>> GetFileUploadList()
+        {
+            var fileUploadList = await _filesUploadRepository.GetFileUploadList();
+            return _mapper.Map<List<FileUploadDTO>>(fileUploadList);
+        }
+        public async Task<FileUploadResponse> AddFileUpload(FileUploadDTO fileUploadDTO)//sourcemodelobject
+        {
+            // 1)Auto mapper is used to create a mapping between  to source model object to destination model object
+            FileUpload obj = new FileUpload();//destinationmodelclass object
+            _mapper.Map(fileUploadDTO,obj);
+            var result = await _filesUploadRepository.AddFileUpload(obj);
+            return result;
+
+        }
+
+        public async Task<FileUploadDTO> GetFileUploadDetailsById(int Id)
+        {
+            var result = await _filesUploadRepository.GetFileUploadDetailsById(Id);
+            FileUploadDTO fileUploadDTO = new FileUploadDTO();
+            fileUploadDTO.ModifiedFilename = result.ModifiedFilename;
+            return fileUploadDTO;
+        }
+    }
+}
